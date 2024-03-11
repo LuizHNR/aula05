81ca67a71f803f437188cb95fe8315ace87cb26a
LuizHNR <luizhneri12@gamil.com>
2024-03-11 08:49:00 -0300

    imc.inicio

---
+++ IMC.py
@@ -0,0 +1,15 @@
+peso = float(input("Digite o seu peso:\n"))
+altura = float(input("Digite a sua altura:\n"))
+imc = (peso / (altura * altura))
+
+print(round(imc, 2))
+
+if imc >= 25:
+    print('gordo')
+
+elif imc >=18 and imc < 25:
+    print('ok')
+
+else:
+    print('magrão')
+
